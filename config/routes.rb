45431816b1e0f976ca34483a579739deeb93f205
Prometheus <berjistechnologies@gmail.com>
2023-06-28 08:03:10 +0300

    Devise custom paths

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
   resources :owners
   get 'commons/search'
   get 'tezo/center'
-  devise_for :users
+  devise_for :user, :path => '', :path_names => { :sign_in => "login", :sign_out => "logout", :sign_up => "register" }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
